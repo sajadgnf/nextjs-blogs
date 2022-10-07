d264c3e0dbada8894b714ebd29c6766e30424f40
Sajad Khazraee Ganjifar <sajadganjifar@gmail.com>
2022-10-07 12:16:45 +0330

    build header UI

--- src/containers/layout/Header.tsx
+++ src/containers/layout/Header.tsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import { AppBar, Box, Stack, Toolbar, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import Link from "next/link";
+import React from "react";
 
 const Heade = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <AppBar
+      elevation={1}
+      sx={{ mb: 8, position: "static", bgcolor: "#fff", color: "#333" }}
+    >
+      <Container maxWidth="lg">
+        <Toolbar>
+          <Box display="flex" alignItems="center" flexGrow={1}>
+            <Link href={"/"}>
+              <a>
+                <Typography ml={3}>Blogs</Typography>
+              </a>
+            </Link>
+
+            <Link href={"/"}>
+              <a>
+                <Typography>Home</Typography>
+              </a>
+            </Link>
+          </Box>
+
+          <Box display="flex" alignItems="center">
+            <Link href={"/"}>
+              <a>
+                <Typography ml={3} mt={.5}>Profile</Typography>
+              </a>
+            </Link>
+
+            <Link href={"/"}>
+              <a>
+                <Typography ml={3}>ورود</Typography>
+              </a>
+            </Link>
+
+            <Link href={"/"}>
+              <a>
+                <Typography>ثبت نام</Typography>
+              </a>
+            </Link>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Heade;
